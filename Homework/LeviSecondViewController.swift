39232fac076b9fc24ccdf4ed883ef6b72cba3579
姜旦旦 <chiangchihhsuan@jiangdandans-MacBook-Pro.local>
2019-03-25 13:39:58 +0800

    Add StoryBorad and ViewController.

---
+++ Homework/LeviSecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LeviSecondViewController.swift
+//  Homework
+//
+//  Created by 姜旦旦 on 2019/3/25.
+//  Copyright © 2019 Jill Yeh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LeviSecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
