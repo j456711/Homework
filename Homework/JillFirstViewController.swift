f090f3c1b6adceb679078bd498b58fb6ca21bcb9
Jill Yeh <j456711282@gmail.com>
2019-03-25 14:52:42 +0800

    Set up rootViewController.

--- Homework/JillFirstViewController.swift
+++ Homework/JillFirstViewController.swift
@@ -10,21 +10,24 @@ import UIKit
 
 class JillFirstViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func nextPageButtonPressed(_ sender: UIButton) {
+    
+        performSegue(withIdentifier: "SegueNextPage", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
--- Homework/JillSecondViewController.swift
+++ Homework/JillSecondViewController.swift
@@ -10,21 +10,23 @@ import UIKit
 
 class JillSecondViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func lastPageButtonPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
